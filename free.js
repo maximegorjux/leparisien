141738a5be1760a699f820cd2ac8f28f87729f85
maximegorjux <maximegorjux@gmail.com>
2021-01-05 15:34:56 +0100

    Update
    - enlève les message newsletter
    - rajoute les photos (ne s'affichaient pas avant a cause du code pop up newsletter)

--- free.js
+++ free.js
@@ -28,10 +28,11 @@
     $(".sticky.below_nav.piano-paywall.relative").remove();
     //ajoute le bouton pour déflouter
     $(".width_full.flex").prepend('<button class="btn btn-outline-warning">Le texte sera déflouté automatiquement.</br>Si le texte reste flou après plusieurs secondes, cliquez ici</button>');
-    //enlève les pop newletter
-    $(".margin_bottom_md").remove();
 
-    //fontion déflouter / enlève bouton
+    //enleve message newletter
+    $(".nl-article-card").remove();
+
+    //fontion déflouter
     $("button").click(function(){
         $(".content").css({"filter": "blur(0px)"});
     });
@@ -39,6 +40,7 @@
     //fonction déflouter le texte - lancée toutes les secondes
     function deFlouter(){
         $(".content").css({"filter": "blur(0px)"});
+
         setTimeout(deFlouter,1000); // rappel après 1 secondes = 1000 millisecondes
     }
 
